738b6abdc0f9243e58bbe3b1c80971acbc93780c
taras <dyshkotaras@gmail.com>
2022-12-30 00:30:05 +0200

    Lessons #39,40

---
+++ app/src/main/java/com/dyshkotaras/messenger/MainActivity.java
@@ -0,0 +1,34 @@
+package com.dyshkotaras.messenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button buttonSendMessage = findViewById(R.id.buttonSendMessage);
+        EditText editTextMessage = findViewById(R.id.EditTextMessage);
+
+        buttonSendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = editTextMessage.getText().toString();
+                launchNextScreen(message);
+            }
+        });
+    }
+
+    private void launchNextScreen(String message) {
+        Intent intent = new Intent(this,ReceivedMessageActivity.class);
+        intent.putExtra("message",message);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/dyshkotaras/messenger/ReceivedMessageActivity.java
@@ -0,0 +1,20 @@
+package com.dyshkotaras.messenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ReceivedMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_received_message);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("message");
+        TextView textViewYourMessage = findViewById(R.id.textViewYourMessage);
+        textViewYourMessage.setText(message);
+    }
+}
